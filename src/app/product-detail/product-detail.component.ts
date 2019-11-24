d3b77960d916342c9f9b598fa8adcf009a980583
Mesqualito <jochen@gebsattel.rocks>
2019-11-24 14:18:19 +0100

    - passing data to routes (2): with property 'snapshot' of Type 'ActivatedRouteSnapshot' from 'ActivatedRoute' the parameters are passed as a "one-time deal", used in scenarios when the parameters passed to the route never changes

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,8 +1,17 @@
 import {Component} from "@angular/core";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'product',
-  template: '<h1 class="product">Product Detail Component</h1>',
+  template: '<h1 class="product">Product Detail for Product: {{productId}}</h1>',
   styles: ['.product { background: cyan }']
 })
-export class ProductDetailComponent {}
+export class ProductDetailComponent {
+  productId: string;
+
+  // the 'ActivatedRoute'-object is injected into the constructor of this component:
+  constructor(route: ActivatedRoute) {
+    this.productId = route.snapshot.paramMap.get('id');
+  }
+
+}
